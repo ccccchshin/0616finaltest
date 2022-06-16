a6fcdf40e6e3d2a682959dee7f98f46b17027f46
石辰栩 <parurusalt308@gmail.com>
2022-06-16 19:21:12 +0800

    fruits完成

---
+++ Web410828608_Final/Web410828608_Final/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410828608_Final.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Exam()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web410828608_Final/Web410828608_Final/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web410828608_Final.MvcApplication" Language="C#" %>
